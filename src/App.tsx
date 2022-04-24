a54b0bfe9d952d77371afbd09080160c400bc470
Markus Muschol <markus.muschol@gmx.de>
2022-04-24 22:11:37 +0200

    Setup service worker

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,27 @@ import logo from './logo.svg'
 import { posts } from './mocks/handlers'
 
 import './App.css'
+interface Post {
+  userId: number
+  id: number
+  title: string
+  body: string
+}
+
 
 function App() {
+  const [posts, setPosts] = useState<Post[]>([])
+  const [isLoading, setIsLoading] = useState(false)
   const [count, setCount] = useState(0)
+
+  const fetchPosts = async() => {
+    setIsLoading(true)
+    await fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(res => res.json())
+      .then(posts => setPosts(posts as Post[]))
+
+    setIsLoading(false)
+  }
 
   return (
     <div className="App">
@@ -16,29 +34,17 @@ function App() {
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
           </button>
+          <button type="button" onClick={() => fetchPosts()}>
+            Fetch Posts { posts.length }
+          </button>
+          <ul>
+          { posts.map((post) => <li>{post.title}</li>)}
+          </ul>
         </p>
         <p>
           Edit <code>App.tsx</code> and save to test HMR updates.
         </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+ 
       </header>
     </div>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client';
 import App from './App'
+import {worker} from './mocks/browser'
+
 import './index.css'
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
+
+if (process.env.NODE_ENV === 'development') {
+  worker.start()
+}
+
 
 root.render(
   <React.StrictMode>
